66da115a6782e6a68c6227910001acc41425f9ad
Sergei Bykov <sabykov2@ya.ru>
2022-11-01 22:17:21 +0600

    Add graphics into

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Take damage for enemy."""
     if char_class == 'warrior':
         return f'{char_name} нанёс урон противнику равный ' \
                f'{5 + random.randint(3, 5)}'
@@ -17,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Give a defence."""
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + random.randint(5, 10)} урона'
     if char_class == 'mage':
@@ -26,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Use skills."""
     if char_class == 'warrior':
         return f'{char_name} применил специальное умение ' \
                f'«Выносливость {80 + 25}»'
@@ -36,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Set character class."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
